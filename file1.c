9740925908a802604be6c50fa1e209b9bf7c5d39
shubhamkaknani <shubhamkaknani654321@fmail.com>
2024-04-21 23:42:48 +0530

    changes added

---
+++ file1.c
@@ -0,0 +1,16 @@
+#include< stdio.h>
+int main()
+{
+	int number1 , number2, sum ;
+	printf("enter two numbers")
+	scanf("%d %d",&number1, &number2);
+
+	sum = number1 + number2;
+	printf("sum of two number is %d",sum);
+}
+int addfunc(int a,intb)
+{
+	int c = a+b;
+	return c;
+}
+}
